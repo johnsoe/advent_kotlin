d477ae7d66abdbf55f39ca3447f4d655058b495b
Evan Johnson <evan.johnson.r@gmail.com>
2023-12-04 11:37:19 -0800

    Beginning 2022 in kotlin

---
+++ src/main/kotlin/2022/01/main.kts
@@ -0,0 +1,22 @@
+import kotlin.Int
+import util.InputParser
+
+val inputParser = InputParser("2022/01/input.txt")
+println(partOne())
+println(partTwo())
+
+fun partOne(): Int {
+    return inputParser.chunk()
+        .map { it.split(" ") }
+        .map { it.sumOf { num -> num.toInt() } }
+        .max()
+}
+
+fun partTwo(): Int {
+    return inputParser.chunk()
+        .map { it.split( " ") }
+        .map { it.sumOf { num -> num.toInt() } }
+        .sortedDescending()
+        .take(3)
+        .sum()
+}
