abcb8634e17653cbbeb8cb9ccf9cf682aad1d5ee
Evan Johnson <evan.johnson.r@gmail.com>
2023-12-05 23:38:18 -0800

    Day 7 2022

---
+++ src/main/kotlin/2022/07/data.kt
@@ -0,0 +1,42 @@
+package `2022`.`07`
+
+sealed class File {
+
+    abstract val size: Long
+    abstract val name: String
+
+    data class Plain (
+        override val size: Long,
+        override val name: String
+    ) : File()
+
+    data class Dir (
+        override val name: String,
+        val parent: Dir?,
+        val files: MutableSet<File> = mutableSetOf()
+    ) : File() {
+        override val size: Long
+            get() = files.sumOf { it.size }
+
+        override fun toString(): String {
+            return name
+        }
+
+        // Necessary to implement hashCode and equals to resolve circular dependency
+        override fun hashCode(): Int {
+            var result = name.hashCode()
+            result = 31 * result + size.hashCode()
+            return result
+        }
+
+        override fun equals(other: Any?): Boolean {
+            if (this === other) return true
+            if (javaClass != other?.javaClass) return false
+
+            other as Dir
+
+            if (name != other.name) return false
+            return size == other.size
+        }
+    }
+}
