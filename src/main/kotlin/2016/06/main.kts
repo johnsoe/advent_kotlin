4ca40400e59b128940226b483fdd7cf78c3c257d
Evan Johnson <evan.johnson.r@gmail.com>
2024-02-14 08:08:50 -0800

    Day 6 2016

---
+++ src/main/kotlin/2016/06/main.kts
@@ -0,0 +1,34 @@
+import kotlin.Int
+import util.InputParser
+
+val inputParser = InputParser("2016/06/input.txt")
+println(partOne())
+println(partTwo())
+
+fun partOne(): String {
+    return generateCharMaps()
+        .map { map ->
+            map.keys.sortedByDescending { map[it] }.first()
+        }.joinToString("")
+}
+
+private fun generateCharMaps(): List<Map<Char, Int>> {
+    return inputParser.lines()
+        .fold(mutableListOf<MutableMap<Char, Int>>()) { acc, s ->
+            s.forEachIndexed { index, c ->
+                if (index >= acc.size) {
+                    acc.add(mutableMapOf())
+                }
+                acc[index].putIfAbsent(c, 0)
+                acc[index][c] = acc[index][c]!! + 1
+            }
+            acc
+        }
+}
+
+fun partTwo(): String {
+    return generateCharMaps()
+        .map { map ->
+            map.keys.sortedBy { map[it] }.first()
+        }.joinToString("")
+}
