4d8041d5a7f428ec49fc919f3617fee565681d3e
Evan Johnson <evan.johnson.r@gmail.com>
2024-08-01 23:16:56 -0700

    Day 6 2015

---
+++ src/main/kotlin/2015/06/data.kt
@@ -0,0 +1,13 @@
+package `2015`.`06`
+
+import java.awt.Point
+
+enum class LightAction {
+    ON, OFF, TOGGLE
+}
+
+data class LightCommand (
+    val action: LightAction,
+    val start: Point,
+    val end: Point
+)
